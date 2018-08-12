fedfda85a002ee89df470af802edaf96b8a345e2
jahona <jahona5240@gmail.com>
2018-08-13 04:05:01 +0900

    function type check with parameter and return type

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-const name = "Nicolas",
-  age = 24,
-  gender = "male";
-
-const sayHi = (name, age, gender) => {
+// (name:string, age:number):return type
+const sayHi = (name: string, age: number, gender: string): void => {
+  /*error return type invalid
+  // return console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+  */
   console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
 };
 
-sayHi(name, age, gender);
+sayHi("bbo", 24, "male");
 
 export {};
