ff456cfeff9cb688c1736a657175f5dc8dac0238
jahona <jahona5240@gmail.com>
2018-08-13 11:57:13 +0900

    function parameter object, object type check using interface

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
-// (name:string, age:number):return type
-const sayHi = (name: string, age: number, gender: string): void => {
-  /*error return type invalid
-  // return console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
-  */
-  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+// interface는 Ts에서만 동작
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "bbo",
+  age: 24,
+  gender: "male"
 };
 
-sayHi("bbo", 24, "male");
+// interface로 person의 타입 체크
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${
+    person.gender
+  }`;
+};
+
+console.log(sayHi(person));
 
 export {};
