2c2a9a0ef7d6e44bc4525dc49dd14e3105ce13fc
jahona <jahona5240@gmail.com>
2018-08-13 13:24:27 +0900

    create block

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,7 @@ class Block {
   }
 }
 
+// 블록체인에 등록된 최초 블록
 const genesisBlock: Block = new Block(0, "201020", "", "Hello", 123456);
 
 let blockchain: Block[] = [genesisBlock];
@@ -40,5 +41,28 @@ const getBlockchain = (): Block[] => blockchain;
 const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+
+  return newBlock;
+};
+
+console.log(createNewBlock("hello"), createNewBlock("bye bye"));
 
 export {};
